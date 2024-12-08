be0b9b576bcf24c0ebc5cc605f2679d9be670545
Priyadharshini Alwar <83493017+priyarshini@users.noreply.github.com>
2024-12-08 14:21:30 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { render } from 'preact'
 import './index.css'
-import { App } from './app.tsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './route/Routes.tsx';
 
-render(<App />, document.getElementById('app')!)
+render(<RouterProvider router={router}/>, document.getElementById('app')!)
